ff79fba58c8e20164f9bb957ee770d9fff2b719e
胡思彬 <1144075799@qq.com>
2019-03-15 12:39:48 +0800

    分类接口

--- lib/config/service_url.dart
+++ lib/config/service_url.dart
@@ -2,4 +2,5 @@ const serviceUrl='http://test.baixingliangfan.cn/baixing/';
 const servicePath={
   'homePageContent':serviceUrl+'wxmini/homePageContent', //商首页店信息
   'homePageBelowConten': serviceUrl+'wxmini/homePageBelowConten', //商城首页热卖商品拉取
+  'getCategory': serviceUrl+'wxmini/getCategory', //商品类别信息
 };
--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import '../service/service_method.dart';
+import 'dart:convert';
 
-class CategoryPage extends StatelessWidget {
+class CategoryPage extends StatefulWidget {
+  _CategoryPageState createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('分类页面'),
-      ),
+    _getCategory();
+    return Container(
+       child: Center(
+         child: Text('分类页面'),
+       ),
     );
+  }
+
+  void _getCategory() async{
+    await request('getCategory').then((val){
+      var data=json.decode(val.toString());
+      print(data);
+    });
   }
 }
