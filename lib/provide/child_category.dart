88aef5d5e57fcc54e8c6141b73312b51bfbdde77
胡思彬 <1144075799@qq.com>
2019-03-18 21:51:51 +0800

    二级导航

--- lib/provide/child_category.dart
+++ lib/provide/child_category.dart
@@ -7,8 +7,14 @@ class ChildCategory with ChangeNotifier{
 
   List<BxMallSubDto> chilCategoryList=[];
 
-  getChildCategory(List list){
-    chilCategoryList=list;
+  getChildCategory(List<BxMallSubDto> list){
+    BxMallSubDto all= BxMallSubDto();
+    all.mallSubId='00';
+    all.mallCategoryId='00';
+    all.mallSubName='全部';
+    all.comments='null';
+    chilCategoryList=[all];
+    chilCategoryList.addAll(list);
     notifyListeners();              //通知听众 局部刷新
   }
 
